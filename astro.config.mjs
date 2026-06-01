a183ac6aa1cd84eeb11b3b22f2986c6dfcd04e53
cbhua <huachuanbo@gmail.com>
2026-06-02 00:50:45 +0900

    Support mdx format report

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
+import mdx from "@astrojs/mdx";
 
 export default defineConfig({
   site: "https://trace-studio.github.io",
-  integrations: [sitemap()],
+  integrations: [sitemap(), mdx()],
   i18n: {
     defaultLocale: "en",
     locales: ["en", "zh"],
